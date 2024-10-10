0ea1a322499ce16c2393e4cae2d12a43565e53df
SeriyEnot0 <maksut04gor@gmail.com>
2024-10-11 00:39:43 +0500

    plus repositoriy

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,6 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  base: '/test/', // Замените на имя вашего репозитория
 })
